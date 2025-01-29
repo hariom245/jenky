df0161f34fa44610d848e5b63e54d8593599945b
hariom245 <123359304+hariom245@users.noreply.github.com>
2025-01-29 22:34:50 +0530

    name changed

--- Manin.java
+++
@@ -1,7 +0,0 @@
-class Mainon{
-    public static void main(String args[]){
-        System.out.println("This is the first java project from vs coce");
-        System.out.println("This is the its example");
-        System.out.println("this is it");
-    }
-}
